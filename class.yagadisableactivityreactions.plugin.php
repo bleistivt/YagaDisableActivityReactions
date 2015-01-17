a644c37c2db058de42e27d975c38cac9ba554bfd
bleistivt <nmh.acww@yahoo.de>
2015-01-17 17:40:49 +0100

    Initial commit

---
+++ class.yagadisableactivityreactions.plugin.php
@@ -0,0 +1,35 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$PluginInfo['YagaDisableActivityReactions'] = array(
+    'Name' => 'Yaga Disable Activity Reactions',
+    'Description' => 'Turns off reactions for activities.',
+    'Version' => '0.1',
+    'RequiredApplications' => array('Yaga' => '1.0'),
+    'MobileFriendly' => true,
+    'Author' => 'Bleistivt',
+    'AuthorUrl' => 'http://bleistivt.net'
+);
+
+class YagaDisableActivityReactionsPlugin extends Gdn_Plugin {
+
+    public function ActivityController_Initialize_Handler($Sender) {
+        $Session = Gdn::Session();
+        $Session->SetPermission('Yaga.Reactions.Add', false);
+
+        if ($Session->User->Admin && is_object($Sender->Head)) {
+            $Sender->Head->AddString(
+                '<style type="text/css">.Item.Activity .Reactions{display:none;}</style>'
+            );
+        }
+    }
+
+    public function ReactionModel_AfterReactionSave_Handler($Sender, $Args) {
+        if ($Args['ParentType'] != 'activity') return;
+
+        Gdn::SQL()->Delete('Reaction', array(
+            'ParentType' => 'activity',
+            'ParentID' => $Args['ParentID']
+        ), 1);
+    }
+
+}
