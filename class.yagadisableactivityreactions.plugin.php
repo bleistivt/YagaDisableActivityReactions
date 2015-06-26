0993fc10c5183b72c26f617d1e7962822837117e
bleistivt <bleistivt@users.noreply.github.com>
2015-06-26 09:52:26 +0200

    simplify for new Yaga version

--- class.yagadisableactivityreactions.plugin.php
+++ class.yagadisableactivityreactions.plugin.php
@@ -1,9 +1,9 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
 
 $PluginInfo['YagaDisableActivityReactions'] = array(
     'Name' => 'Yaga Disable Activity Reactions',
     'Description' => 'Turns off reactions for activities.',
-    'Version' => '0.2',
+    'Version' => '0.3',
     'RequiredApplications' => array('Yaga' => '1.0'),
     'MobileFriendly' => true,
     'Author' => 'Bleistivt',
@@ -13,25 +13,21 @@ $PluginInfo['YagaDisableActivityReactions'] = array(
 
 class YagaDisableActivityReactionsPlugin extends Gdn_Plugin {
 
-    public function ActivityController_Initialize_Handler($Sender) {
-        $Session = Gdn::Session();
-        $Session->SetPermission('Yaga.Reactions.Add', false);
+    public function activityController_initialize_handler($sender) {
+        $session = Gdn::session();
+        $session->setPermission('Yaga.Reactions.Add', false);
 
-        if ($Session->User->Admin && is_object($Sender->Head)) {
-            $Sender->Head->AddString(
+        if ($session->User->Admin) {
+            $sender->Head->addString(
                 '<style type="text/css">.Item.Activity .Reactions{display:none;}</style>'
             );
         }
     }
 
-    public function ReactionModel_AfterReactionSave_Handler($Sender, $Args) {
-        if ($Args['ParentType'] != 'activity') return;
-
-        Gdn::SQL()->Delete('Reaction', array(
-            'ParentType' => 'activity',
-            'ParentID' => $Args['ParentID']
-        ), 1);
-        Yaga::GivePoints($Args['ParentUserID'], -1 * $Args['Points'], 'Reaction');
+    public function ReactionModel_AfterReactionSave_Handler($sender, $args) {
+        if ($args['ParentType'] == 'activity'){
+            throw permissionException();
+        }
     }
 
 }
